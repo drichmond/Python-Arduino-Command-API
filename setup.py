cdaec66696df42561fb34f16b3093eee25053044
Dustin Richmond <drichmond@eng.ucsd.edu>
2017-09-06 17:16:30 -0600

    Adding pynq-specific modifications to distribute prototype.ino

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+import os
+import site
+
+board = os.environ['BOARD']
+board_folder = 'boards/{}/'.format(board)
+pynq_data_files = [(os.path.join('{}/pynq/overlays'.format(site.getsitepackages()[0]), root.replace(board_folder, '')),
+                    [os.path.join(root, f) for f in files]) for root, dirs, files in os.walk(board_folder)]
 setup(name='arduino-python',
       version='0.2',
       install_requires=['pyserial'],
@@ -8,6 +15,13 @@ setup(name='arduino-python',
       author='Tristan Hearn',
       author_email='tristanhearn@gmail.com',
       url='https://github.com/thearn/Python-Arduino-Command-API',
+      package_data= {
+          '': ['sketches/prototype/prototype.ino', 'notebooks/arduino_examples.ipynb'],
+      },
       license='MIT',
       packages=['Arduino'],
-      )
+      data_files = [('/home/xilinx/jupyter_notebooks/sensors96b/',
+                     ['sketches/prototype/prototype.ino',
+                      'notebooks/arduino_examples.ipynb',
+                      'sketches/prototype/makefile'])],
+)
